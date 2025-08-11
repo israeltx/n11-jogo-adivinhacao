73049cfc3d2b854b9cc0fca3cca4402714dd91b1
Israel Teixeira <israeltx@gmail.com>
2025-08-11 15:44:33 -0300

    Create variations of the letter component

--- src/components/LettersUsed/index.tsx
+++ src/components/LettersUsed/index.tsx
@@ -1,9 +1,17 @@
 import styles from "./styles.module.css"
 
-export function LettersUsed(params:type) {
+import { Letter } from "../Letter"
+
+export function LettersUsed() {
   return (
     <div className={styles.lettersUsed}>
       <h5>Letras utilizadas</h5>
+
+      <div>
+        <Letter value="X" size="small"/>
+        <Letter value="X" size="small" color="correct"/>
+        <Letter value="X" size="small" color="wrong"/>
+      </div>
     </div>
   )
 }
