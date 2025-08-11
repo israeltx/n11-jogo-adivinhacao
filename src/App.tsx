379cae8906dfa56b05bfb2b31658d9b6e64b3998
Israel Teixeira <israeltx@gmail.com>
2025-08-11 14:02:23 -0300

    Create the tips component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Header } from "./components/Header"
+import { Tip } from "./components/Tip"
 
 import styles from "./app.module.css"
 
@@ -12,6 +13,7 @@ export default function App() {
     <div className={styles.container}>
       <main>
         <Header current={5} max={10} onRestart={handleRestartGame}/>
+        <Tip tip="Umas das linguagens de programação dinâmica mais utilizadas"/>
       </main>
     </div>
   )
