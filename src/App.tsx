84cfd8a690c1368183e503781f1150268962189d
Israel Teixeira <israeltx@gmail.com>
2025-08-11 14:13:23 -0300

    Create the letters component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Header } from "./components/Header"
 import { Tip } from "./components/Tip"
+import { Letter } from "./components/Letter"
 
 import styles from "./app.module.css"
 
@@ -14,6 +15,14 @@ export default function App() {
       <main>
         <Header current={5} max={10} onRestart={handleRestartGame}/>
         <Tip tip="Umas das linguagens de programação dinâmica mais utilizadas"/>
+        
+        <div className={styles.word}>
+          <Letter value="R"/>
+          <Letter value="E"/>
+          <Letter value="A"/>
+          <Letter value="C"/>
+          <Letter value="T"/>
+        </div>
       </main>
     </div>
   )
